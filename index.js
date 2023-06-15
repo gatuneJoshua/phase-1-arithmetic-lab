92dcd0342a2b56751b6fcf5d3907fce1de111849
Joshua <joshua.gatune@student.moringaschool.com>
2023-06-15 12:04:53 +0300

    my commits

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+const num1 = 31;
 
-// Write your code here
+const num2 = 2;
+
+const multiply = (num1 * num2);
+
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+
+const num3 = 19;
+
+const num4 = 5;
+
+const mod = (num3 % num4);
+
+const max = Math.max(20, 17, 14, 11, 8);
+
